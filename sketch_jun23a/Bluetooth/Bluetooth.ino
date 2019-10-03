167162a7a1f8f9a3891528b89429ec3377c82a9b
yuri abaev <yuri.abaev@gmail.com>
2019-10-03 14:47:47 +0300

    added custom watering and setting watering duration

--- sketch_jun23a/Bluetooth/Bluetooth.ino
+++ sketch_jun23a/Bluetooth/Bluetooth.ino
@@ -1,3 +1,5 @@
+#include <Motor.h>
+
 
 /*Program to control LED (ON/OFF) from ESP32 using Serial Bluetooth
  * Thanks to Neil Kolbans for his efoorts in adding the support to Arduino IDE
@@ -6,34 +8,50 @@
 
 #include "BluetoothSerial.h" //Header File for Serial Bluetooth, will be added by default into Arduino
 #include <Motor.h>
+#include <MillisConverter.h>
+#include "StringSplitter.h"
 
 Motor motor(2);
+MillisConverter millisConverter;
 
 BluetoothSerial ESP_BT; //Object for Bluetooth
 
 int incoming;
-String command;
 int LED_BUILTIN = 2;
 
+///
+int WateringDuration = 3000;
 
 String getCommand() {
   String command="";
   while(ESP_BT.available()) //Check if we receive anything from Bluetooth
   {
-    char  c=ESP_BT.read();
-    command+=String(c);
-   
+//    char  c=ESP_BT.read();
+//    command+=String(c);
+ command=ESP_BT.readString();
   };
   return command;
 }
 
 
+void setWateringDuration(int time) {
+    WateringDuration = time;
+}
 void doLedOn() {
     //digitalWrite(LED_BUILTIN, HIGH);
     motor.on();
     ESP_BT.println("LED turned ON");
 }
 
+void doCustomWater() {
+    motor.on();
+    ESP_BT.println("CUSTOM turned ON");
+    //delay(settings.getWateringDuration());
+    delay(WateringDuration);
+    motor.off();
+    ESP_BT.println("CUSTOM turned OFF");
+}
+ 
 void doLedOff() {
    //digitalWrite(LED_BUILTIN, LOW);
    motor.off();
@@ -49,15 +67,33 @@ void setup() {
 }
 
 void loop() {
-  command=getCommand();
 
-  if(command.length() !=0){
-    Serial.println(motor.isOn() );
+  
+   String fullCommand;//=getCommand();
+  if(ESP_BT.available()){
+     fullCommand=ESP_BT.readString();
+      Serial.print("fullCommand1:");Serial.println(fullCommand);
+  }
+  if(fullCommand.length() !=0){
+      StringSplitter *splitter = new StringSplitter(fullCommand, ' ', 3);
+
+      String command = splitter->getItemAtIndex(0);
+      String arg = splitter->getItemAtIndex(1);
+      
+      Serial.print("splitter->getItemCount():");Serial.println(splitter->getItemCount());
+       Serial.print("fullCommand:");Serial.println(fullCommand);
+       Serial.print("command:");Serial.println(command);
+       Serial.print("arg:");Serial.println(arg);
+
     Serial.print("Received:");Serial.println(command );
-    if (command== "on") {
-        doLedOn();
-    }else if (command== "off") {
-        doLedOff();
+    if (command== "water") {
+        doCustomWater();
+    }else if (command== "set_watering_duration") {
+        setWateringDuration(arg.toInt());
+    }else if (command== "get_watering_duration") {
+         ESP_BT.println(WateringDuration);
+    }else if (command== "time") {
+        ESP_BT.println(millisConverter.convert( millis()/1000));
     }else{
         ESP_BT.println("no such command");
     }    
